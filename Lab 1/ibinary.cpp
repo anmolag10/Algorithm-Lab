4373ef24ad9f78c5e0648400a65b6f5523b784d6
Anmol Agarwal <57227202+anmolag10@users.noreply.github.com>
2021-05-03 13:36:18 +0530

    Update ibinary.cpp

--- Lab 1/ibinary.cpp
+++ Lab 1/ibinary.cpp
@@ -1,33 +1,42 @@
 #include<iostream>
 using namespace std;
 
-
+static int count=0;
 int binarysearch(int key,int size, int arr[])
 {
+  count++;
   int mid, low, high;
-  low=0;
-  high=size-1;
+  low=0; count++; 
+  high=size-1; count++;
   while(low<high)
-	{
-	    mid= high+low/2;
-	    if(arr[mid]==key)
-		return mid;
+	{   count++;
+	    mid= high+low/2; count++;
+	    if(arr[mid]>key)
+        { count++;
+          count++;
+          high = mid-1;
+		 
+        }
 	    else if( arr[mid]<key)
-		{
+		{   count++;
 			low=mid+1;
+            count++;
 		}
 	else
-		{
-			high = mid-1;
+		{  
+			
+           return mid;
 		}
              
 	}
 
-
+return -1;
 }
 int main()
-{ int arr[10] = { 0,1,2,3,4,5,6,7,8,9};
+{ int arr[10] = {10,20,30};
   
-  cout<<binarysearch(5,10,arr);
+  cout<<binarysearch(10,10,arr)<<endl;
+  cout<<"Count:"<<count<<endl;
 }
+   
    
