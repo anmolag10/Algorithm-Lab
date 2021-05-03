c56cdcaf7e6d8d767b2484a27383c1e99a0a4b90
Anmol Agarwal <57227202+anmolag10@users.noreply.github.com>
2021-05-03 13:40:29 +0530

    Update rbinary.cpp

--- Lab 1/rbinary.cpp
+++ Lab 1/rbinary.cpp
@@ -1,25 +1,33 @@
 #include<iostream>
 using namespace std;
 
+int count=0;
 
 int rbinarysearch(int arr[], int low, int high,int key)
 { 
-  int mid=(low+high)/2;
-  if(low<high) {
+  int mid=(low+high)/2; count++;
+  if(low<high) { count++;
      
   if ( arr[mid]==key)
+  { count++;
 	return mid;
+  }
  
   else if ( arr[mid]<key)
+  {
       rbinarysearch(arr,mid+1,high,key);
+      count++;
+  }
   else 
-   {rbinarysearch(arr,low,mid-1,key); }
+   {rbinarysearch(arr,low,mid-1,key); 
+    count++;}
+
     
                 }
 }
 int main()
-{ int arr[10] = { 0,1,2,3,4,5,6,7,8,9};
+{ int arr[10] = { 10,20,30};
   
-  cout<<rbinarysearch(arr,0,9,5);
+  cout<<rbinarysearch(arr,0,2,20)<<endl;
+  cout<<"Count: "<<count;
 }
-   
